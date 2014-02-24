5659e8380e90560a4f20a7784221afac07dfe02f
Dave Horton <daveh@beachdognet.com>
2014-02-24 11:58:20 -0500

    test app fix

--- examples/connect-1.js
+++ examples/connect-1.js
@@ -17,7 +17,7 @@ app.connect( config ) ;
 
 app.invite(function(req, res) {
 
-	msmlApp.makeConnection('192.168.173.139', {
+	msmlApp.makeConnection('209.251.49.158', {
 		remote: {
 			sdp: req.body
 			,'content-type': req.get('content-type')

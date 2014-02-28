b2420803aefda5ff513eef26f46d45e298180540
Dave Horton <daveh@beachdognet.com>
2014-02-27 22:11:51 -0500

    dialog revamp

--- examples/play-2.js
+++ examples/play-2.js
@@ -46,50 +46,53 @@ app.on('sipdialog:terminate', function(e) {
 
 function playFile( conn ) {
 
-	var dialog = new conn.Dialog() ;
-	dialog.add('play', {
-		barge: true
-		,cleardb: false
-		,audio: {
-			uri:'file://provisioned/4300.wav'
-		}
-		,playexit: {
-			send: {
-				target:'source'
-				,event:'app.playDone'
-				,namelist: 'play.amt play.end'
+	conn.makeDialog({
+		'play', {
+			barge: true
+			,cleardb: false
+			,audio: {
+				uri:'file://provisioned/4300.wav'
+			}
+			,playexit: {
+				send: {
+					target:'source'
+					,event:'app.playDone'
+					,namelist: 'play.amt play.end'
+				}
 			}
 		}
-	})
-	.add('dtmf', {
-		fdt: '10s'
-		,idt: '6s'
-		,edt: '6s'
-		,cleardb: false
-		,pattern: {
-			digits: 'min=4;max=10;rtk=#'
-			,format: 'moml+digits'
-		}
-		,dtmfexit: {
-			send: {
-				target:'source'
-				,event:'app.dtmfDone'
-				,namelist: 'dtmf.digits dtmf.end'
+		'dtmf', {
+			fdt: '10s'
+			,idt: '6s'
+			,edt: '6s'
+			,cleardb: false
+			,pattern: {
+				digits: 'min=4;max=10;rtk=#'
+				,format: 'moml+digits'
+			}
+			,dtmfexit: {
+				send: {
+					target:'source'
+					,event:'app.dtmfDone'
+					,namelist: 'dtmf.digits dtmf.end'
+				}
 			}
 		}
-	}) 
-	.start( function( err, req, res) {
+	}, function(err, dialog ) {
 		if( err ) throw err ;
-		debug('media dialog started successfully') ;
+		debug('media dialog started (callback)') ;
 	}) ;
 }
 
+app.on('msml:dialog:create', function(e) {
+	debug('media dialog started (event)') ;
+}) ;
+app.on('msml:dialog:terminate', function(e) {
+	debug('media dialog terminated') ;
+}) ;
 app.on('msml:dialog:playDone', function(e) {
 	debug('playDone, reason: %s, amount played: %s', e['play.end'], e['play.amt']) ;
 }) ;
 app.on('msml:dialog:dtmfDone', function(e) {
 	debug('dtmfDone, reason: %s, digits: %s', e['dtmf.end'], e['dtmf.digits']) ;
-}) ;
-app.on('msml:dialog:exit', function(e) {
-	debug('my play dialog exited') ;
 }) ;
